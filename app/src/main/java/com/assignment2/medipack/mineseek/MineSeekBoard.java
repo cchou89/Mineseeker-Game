3ee34c7539c7219d21816ae424e63c1c9d7a73e5
ckc21 <ckc21@sfu.ca>
2017-02-21 15:51:41 -0800

    Initial Commit

---
+++ app/src/main/java/com/assignment2/medipack/mineseek/MineSeekBoard.java
@@ -0,0 +1,8 @@
+package com.assignment2.medipack.mineseek;
+
+/**
+ * Created by Medipack on 2017-02-20.
+ */
+
+public class MineSeekBoard {
+}
---
+++ app/src/main/java/com/assignment2/medipack/mineseek/MineSeekGame.java
@@ -0,0 +1,13 @@
+package com.assignment2.medipack.mineseek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MineSeekGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_seek_game);
+    }
+}
