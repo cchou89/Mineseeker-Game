d53298d5dc8f1a893e327d7cc10ccd0390f7181c
ckc21 <ckc21@sfu.ca>
2017-02-23 21:07:08 -0800

    Game Logic skeleton

--- app/src/main/java/com/assignment2/medipack/mineseek/MainMenu.java
+++ app/src/main/java/com/assignment2/medipack/mineseek/MainMenu.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
+    private MineSeekBoard gameBoard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/assignment2/medipack/mineseek/mine.java
+++ app/src/main/java/com/assignment2/medipack/mineseek/mine.java
@@ -4,5 +4,47 @@ package com.assignment2.medipack.mineseek;
  * Created by Medipack on 2017-02-20.
  */
 
-public class mine {
+public class Mine {
+    private boolean isMine;
+    private int nearbyMines;
+    private int rowCoord;
+    private int colCoord;
+
+    Mine(){
+        isMine = false;
+        nearbyMines = 0;
+        rowCoord = 0;
+        colCoord = 0;
+    }
+
+    Mine(boolean mineStatus){
+        isMine = mineStatus;
+        nearbyMines = 1;
+    }
+
+    public int getNearbyMines() {
+        return nearbyMines;
+    }
+
+    public boolean getMineStatus() {
+        return isMine;
+    }
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+
+    public void setNearbyMines(int nearbyMines) {
+        this.nearbyMines = nearbyMines;
+    }
+
+    public void setColCoord(int colCoord) {
+        this.colCoord = colCoord;
+    }
+
+    public void setRowCoord(int rowCoord) {
+        this.rowCoord = rowCoord;
+    }
 }
+
+
